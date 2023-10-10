7cc23342bab3c66b64fba5fc228b718e8b901f37
IIIgorMoura <igor2007moura@gmail.com>
2023-10-10 14:23:52 -0300

    servicos

--- 02/main.js
+++ 02/main.js
@@ -13,6 +13,7 @@ trocarTema.addEventListener('click', () => {
         document.documentElement.style.setProperty('--corFundoDestaque', '#b112d9');
         document.documentElement.style.setProperty('--corTexto-2', '#000');
         document.documentElement.style.setProperty('--fundoImgLanding', '#09ff00');
+        document.documentElement.style.setProperty('--corTextoLaranja', '#ff9310');
     }
     
     else if (trocarTema.classList.contains('temaClaro-Botao')) {
@@ -23,5 +24,6 @@ trocarTema.addEventListener('click', () => {
         document.documentElement.style.setProperty('--corFundoDestaque', '#464646');
         document.documentElement.style.setProperty('--corTexto-2', '#fff');
         document.documentElement.style.setProperty('--fundoImgLanding', '#008cff');
+        document.documentElement.style.setProperty('--corTextoLaranja', '#09ff00');
     }
 });
